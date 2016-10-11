3cf897e1ff1cbfb40832c0202ebb1a9444f692f7
majiajun <majiajun@unitedstack.com>
2016-10-11 10:58:38 +0800

    object storage backend init
    Change-Id: I144f492826759233d0a2fe4e54f26e0aab757c4a
    Type: NF
    Jira: HALO-1099

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  setting: [{
+    app: 'global',
+    name: 'enable_object_storage',
+    value: 'false',
+    type: 'boolean'
+  }]
+};
---
+++ views/object_storage/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const View = require('views/base');
+
+function main(app, clientApps, currentView, viewPlugins) {
+  let views = app.get('views');
+  views.push(__dirname);
+  const view = new View(app, clientApps, currentView, viewPlugins);
+  view.init();
+}
+
+function haloProcessor(user, HALO) {
+  HALO.application.application_list = HALO.application.application_list.filter(e => {
+    return HALO.settings.enable_object_storage ? true : e !== 'object_storage';
+  });
+}
+
+module.exports = {
+  main: main,
+  haloProcessor: haloProcessor
+};
