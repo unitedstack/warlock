78b8a09211469d7846d6d3939680c9593114645d
majiajun <majiajun@unitedstack.com>
2016-12-08 17:36:00 +0800

    add description in setting
    Change-Id: I2990ae637d13bbc4a5800352c951ef9cbb90db87
    Type: NF
    Jira: HALO-1365

--- config.sample.js
+++ config.sample.js
@@ -5,6 +5,13 @@ module.exports = {
     app: 'global',
     name: 'enable_object_storage',
     value: 'false',
-    type: 'boolean'
+    type: 'boolean',
+    description: '开启对象存储功能'
+  }, {
+    app: 'storage',
+    name: 'view.css',
+    value: '',
+    type: 'text',
+    description: '对象存储-样式定制'
   }]
 };
